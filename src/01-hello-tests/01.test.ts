e78719a88a42df8fe25300a03b93ee62eee087be
Roman <romantut1988@mail.ru>
2023-07-24 16:49:12 +0300

    remove idea

--- src/01-hello-tests/01.test.ts
+++ src/01-hello-tests/01.test.ts
@@ -2,7 +2,7 @@ import {splitIntoWords} from "./01";
 
 test("splitting into words should be correct", () => {
     const sent1 = "Hello my friend!"
-    const sent2 = "JS - the best programming language."
+    const sent2 = "JS - the best programmings language."
 
     const result1 = splitIntoWords(sent1)
     const result2 = splitIntoWords(sent2)
@@ -16,7 +16,7 @@ test("splitting into words should be correct", () => {
     expect(result2[0]).toBe("js")
     expect(result2[1]).toBe("the")
     expect(result2[2]).toBe("best")
-    expect(result2[3]).toBe("programming")
+    expect(result2[3]).toBe("programmings")
     expect(result2[4]).toBe("language")
 })
 
